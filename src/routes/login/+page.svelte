282239eca63e9f173e835a117e57c28bf5138080
Vivek Sharma <er.viveksharma2008@gmail.com>
2025-01-21 15:35:02 +0530

    Add Modal component

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,89 +1,119 @@
 <script lang="ts">
-    import Logo from "$lib/components/general/Logo.svelte";
-    import { AuthService } from '$lib/services/auth-service';
-    import LoadingButton from "$lib/components/form/buttons/LoadingButton.svelte";
-	import { login } from "$lib/stores/authStore";
-	import Errors from "$lib/components/form/messages/Errors.svelte";
+	import Logo from '$lib/components/general/Logo.svelte';
+	import { AuthService } from '$lib/services/auth-service';
+	import LoadingButton from '$lib/components/form/buttons/LoadingButton.svelte';
+	import { login } from '$lib/stores/authStore';
+	import Errors from '$lib/components/form/messages/Errors.svelte';
+	import Modal from '$lib/components/ui/modal/Modal.svelte';
+	let showModal = false;
+	const handleToggleModal = () => {
+		showModal = !showModal;
+	};
 
-    let email = '';
-    let password = '';
-    let errorMessages: string[] = [];
-    let isLoading = false;
+	let email = '';
+	let password = '';
+	let errorMessages: string[] = [];
+	let isLoading = false;
 
-    const authService = new AuthService();
+	const authService = new AuthService();
 
-    async function handleSubmit(event: Event) {
-        event.preventDefault();
-        isLoading = true;
+	async function handleSubmit(event: Event) {
+		event.preventDefault();
+		isLoading = true;
 
-        try {
-            const data = await authService.login({ email, password });
-            if (data.success) {
-                login(data.access_token);
-            } else {
-                handleErrors(data);
-            }
-        } catch (error) {
-            errorMessages.push('An unexpected error occurred.');
-        } finally {
-            isLoading = false;
-        }
-    }
+		try {
+			const data = await authService.login({ email, password });
+			if (data.success) {
+				login(data.access_token);
+			} else {
+				handleErrors(data);
+			}
+		} catch (error) {
+			errorMessages.push('An unexpected error occurred.');
+		} finally {
+			isLoading = false;
+		}
+	}
 
-    function handleErrors(data: any) {
-        errorMessages = [];
+	function handleErrors(data: any) {
+		errorMessages = [];
 
-        if (data.errors) {
-            Object.keys(data.errors).forEach(key => {
-                errorMessages.push(...data.errors[key]);
-            });
-        } else {
-            errorMessages.push(data.message || 'An error occurred');
-        }
-    }
+		if (data.errors) {
+			Object.keys(data.errors).forEach((key) => {
+				errorMessages.push(...data.errors[key]);
+			});
+		} else {
+			errorMessages.push(data.message || 'An error occurred');
+		}
+	}
 </script>
 
 <div class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <a href="/" aria-label="Home">
-                <Logo />
-            </a>
-        </div>
+	<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+		<div>
+			<button on:click={() => handleToggleModal()}>Open modal</button>
+			<Modal title="Edit your details" open={showModal} on:close={() => handleToggleModal()}>
+				<svelte:fragment slot="body">This is content inside my modal! 👋</svelte:fragment>
+			</Modal>
+			<a href="/" aria-label="Home">
+				<Logo />
+			</a>
+		</div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <Errors {errorMessages} />
-            <form on:submit={handleSubmit}>
-                <div>
-                    <label class="block font-medium text-sm text-gray-700" for="email">Email</label>
-                    <input class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full" id="email" type="email" bind:value={email} required>
-                </div>
-                <div class="mt-4">
-                    <label class="block font-medium text-sm text-gray-700" for="password">Password</label>
-                    <input class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full" id="password" type="password" bind:value={password} required autocomplete="current-password">
-                </div>
+		<div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+			<Errors {errorMessages} />
+			<form on:submit={handleSubmit}>
+				<div>
+					<label class="block font-medium text-sm text-gray-700" for="email">Email</label>
+					<input
+						class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full"
+						id="email"
+						type="email"
+						bind:value={email}
+						required
+					/>
+				</div>
+				<div class="mt-4">
+					<label class="block font-medium text-sm text-gray-700" for="password">Password</label>
+					<input
+						class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full"
+						id="password"
+						type="password"
+						bind:value={password}
+						required
+						autocomplete="current-password"
+					/>
+				</div>
 
-                <div class="block mt-4">
-                    <label for="remember_me" class="flex items-center">
-                        <input type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" id="remember_me" name="remember">
-                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
+				<div class="block mt-4">
+					<label for="remember_me" class="flex items-center">
+						<input
+							type="checkbox"
+							class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							id="remember_me"
+							name="remember"
+						/>
+						<span class="ml-2 text-sm text-gray-600">Remember me</span>
+					</label>
+				</div>
 
-                <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="/forgot-password">
-                        Forgot your password?
-                    </a>
-                    
-                    {#if isLoading}
-                        <LoadingButton buttonText="Log in" />
-                    {:else}
-                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4">
-                            Log in
-                        </button>
-                    {/if}
-                </div>
-            </form>
-        </div>
-    </div>
+				<div class="flex items-center justify-end mt-4">
+					<a class="underline text-sm text-gray-600 hover:text-gray-900" href="/forgot-password">
+						Forgot your password?
+					</a>
+
+					{#if isLoading}
+						<LoadingButton buttonText="Log in" />
+					{:else}
+						<button
+							type="submit"
+							class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4"
+						>
+							Log in
+						</button>
+					{/if}
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
