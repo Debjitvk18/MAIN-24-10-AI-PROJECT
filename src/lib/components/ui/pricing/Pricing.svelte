8e1a16c6248b59824655c2a9d552b253a945e27c
Vivek Sharma <er.viveksharma2008@gmail.com>
2025-01-22 15:14:40 +0530

    Pricing Component code refactor

--- src/lib/components/ui/pricing/Pricing.svelte
+++ src/lib/components/ui/pricing/Pricing.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { SubscriptionService } from '$lib/services/subscription-service';
 	import Loader from '../spinners/Loader.svelte';
+	import PricingPlan from './PricingPlan.svelte';
 
 	let data = {};
 	let loading = false;
@@ -61,95 +62,7 @@
 			class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2"
 		>
 			{#each data as plan}
-				{#if !plan.is_recommended}
-					<div
-						class="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:p-10 lg:mx-0 lg:rounded-bl-3xl"
-					>
-						<h3 class="text-base font-semibold text-[#2C7BE5]">{plan.name}</h3>
-						<p class="mt-4 flex items-baseline gap-x-2">
-							<span class="text-5xl font-semibold tracking-tight text-gray-900"
-								>{plan.currency_symbol}{plan.charge}</span
-							>
-							<span class="text-base text-gray-500">/{plan.billing_frequency}</span>
-						</p>
-						<p class="mt-6 text-base/7 text-gray-600">
-							{plan.caption}
-						</p>
-						{#if plan.features}
-							<ul role="list" class="mt-8 space-y-3 text-sm/6 text-gray-600 sm:mt-10">
-								{#each plan.features as feature}
-									<li class="flex gap-x-3">
-										<svg
-											class="h-6 w-5 flex-none text-[#2C7BE5]"
-											viewBox="0 0 20 20"
-											fill="currentColor"
-											aria-hidden="true"
-											data-slot="icon"
-										>
-											<path
-												fill-rule="evenodd"
-												d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-												clip-rule="evenodd"
-											/>
-										</svg>
-										{feature}
-									</li>
-								{/each}
-							</ul>
-						{/if}
-						<a
-							href="#"
-							aria-describedby="tier-hobby"
-							class="mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-[#2C7BE5] ring-1 ring-inset ring-indigo-200 hover:text-[#FFFFFF] hover:bg-[#2C7BE5] hover:ring-[#2C7BE5] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#2C7BE5] sm:mt-10"
-						>
-							Get started today
-						</a>
-					</div>
-				{:else}
-					<div
-						class="relative rounded-3xl bg-gray-900 p-8 shadow-2xl ring-1 ring-gray-900/10 sm:p-10"
-					>
-						<h3 id="tier-enterprise" class="text-base/7 font-semibold text-[#2C7BE5]">
-							{plan.name}
-						</h3>
-						<p class="mt-4 flex items-baseline gap-x-2">
-							<span class="text-5xl font-semibold tracking-tight text-white"
-								>{plan.currency_symbol}{plan.charge}</span
-							>
-							<span class="text-base text-gray-400">/{plan.billing_frequency}</span>
-						</p>
-						<p class="mt-6 text-base/7 text-gray-300">
-							{plan.caption}
-						</p>
-						{#if plan.features}
-							<ul role="list" class="mt-8 space-y-3 text-sm/6 text-gray-300 sm:mt-10">
-								{#each plan.features as feature}
-									<li class="flex gap-x-3">
-										<svg
-											class="h-6 w-5 flex-none text-[#2C7BE5]"
-											viewBox="0 0 20 20"
-											fill="currentColor"
-											aria-hidden="true"
-											data-slot="icon"
-											><path
-												fill-rule="evenodd"
-												d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-												clip-rule="evenodd"
-											></path></svg
-										>
-										{feature}
-									</li>
-								{/each}
-							</ul>
-						{/if}
-						<a
-							href="#"
-							aria-describedby="tier-enterprise"
-							class="mt-8 block rounded-md bg-[#2C7BE5] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-[#000000] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#2C7BE5] hover:ring-1 hover:ring-[#2C7BE5] sm:mt-10"
-							>Get started today</a
-						>
-					</div>
-				{/if}
+				<PricingPlan {plan} />
 			{/each}
 		</div>
 	{/if}
