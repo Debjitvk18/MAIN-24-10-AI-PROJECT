bd7c4c309abdbdc75a2a786f42a83fc1dcc1d693
Vivek Sharma <erviveksharma2008@gmail.com>
2025-01-23 18:48:43 +0530

    hero section redesign

--- src/lib/components/ui/pricing/Pricing.svelte
+++ src/lib/components/ui/pricing/Pricing.svelte
@@ -26,19 +26,11 @@
 	});
 </script>
 
-<div class="py-40">
-	<div
-		class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
-		aria-hidden="true"
-	>
-		<div
-			class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-		></div>
-	</div>
+<div class="py-40" id="pricing">
+
 	<div class="mx-auto max-w-4xl text-center">
 		<h2 class="text-base/7 font-semibold text-[#2C7BE5]">Pricing</h2>
-		<p class="mt-2 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
+		<p class="mt-2 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
 			Choose the right plan for you
 		</p>
 	</div>
