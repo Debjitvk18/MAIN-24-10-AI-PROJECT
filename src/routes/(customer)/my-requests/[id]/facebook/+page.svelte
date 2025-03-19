e2f016184d35349ec4bca3bab8199b6f0e32a856
Vivek Sharma <er.viveksharma2008@gmail.com>
2025-03-19 06:26:44 +0000

    Display FacebookPosts in dashboard

--- src/routes/(customer)/my-requests/[id]/facebook/+page.svelte
+++ src/routes/(customer)/my-requests/[id]/facebook/+page.svelte
@@ -9,6 +9,9 @@
 	import TableShimmer from '$lib/components/general/shimmer/TableShimmer.svelte';
 	import { formatDate } from '$lib/utils/generalUtils';
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import FacebookPostCollection from '$lib/components/ui/map/social-cards/facebook/FacebookPostCollection.svelte';
+	import CardShimmer from '$lib/components/general/shimmer/CardShimmer.svelte';
+	import FacebookIconImg from '$lib/assets/svg/marker/facebook-pin.svg';
 
 	const slug = 'facebook';
 	$: id = $page.params.id;
@@ -24,7 +27,93 @@
 				throw new Error(res.error);
 			}
 
-			posts = [...(res?.response?.response?.results || [])];
+			posts = res.response.reduce((acc, response) => {
+				const data = response.response;
+				const fbData = Object.entries(data)[0];
+				const fbType = fbData[0];
+				const fbObjects = fbData[1];
+
+				const getFbDetails = (fbObject) => {
+					const commonDetails = {
+						id: fbObject.id,
+						url: fbObject.url ?? '#',
+						type: fbType,
+						historical: data?.historical || false
+					};
+
+					switch (fbType) {
+						case 'posts':
+							return {
+								...commonDetails,
+								content: fbObject.message,
+								postMedia: fbObject.attachments?.[0]?.preview_image || FacebookIconImg,
+								userName: fbObject.actors?.[0]?.name,
+								userProfilePhoto: fbObject.actors?.[0]?.profile_picture,
+								lat: null,
+								lng: null,
+								postTime: fbObject.creation_time,
+								reaction: fbObject.feedback?.reaction_count,
+								comment: fbObject.feedback?.comment_count,
+								share: fbObject.feedback?.share_count,
+								like: fbObject.feedback?.like_count,
+								postType: fbObject.attachments?.[0]?.attachment_type
+							};
+						case 'users':
+							return {
+								...commonDetails,
+								name: fbObject.name,
+								image: fbObject.photoUrl,
+								info: fbObject.info
+							};
+						case 'pages':
+							return {
+								...commonDetails,
+								name: fbObject.name,
+								image: fbObject.photoUrl,
+								info: fbObject.info,
+								postsFrequency: fbObject.postsFrequency,
+								followers: fbObject.followers
+							};
+						case 'groups':
+							return {
+								...commonDetails,
+								name: fbObject.name,
+								image: fbObject.photoUrl,
+								info: fbObject.info,
+								postsFrequency: fbObject.postsFrequency,
+								members: fbObject.members
+							};
+						case 'events':
+							return {
+								...commonDetails,
+								name: fbObject.name,
+								image: fbObject.picture,
+								attendings: fbObject.attendings,
+								startTimeStamp: fbObject.startTimeStamp,
+								startText: fbObject.startText,
+								timezone: fbObject.timezone,
+								isAllDay: fbObject.isAllDay,
+								endTimeStamp: fbObject.endTimeStamp,
+								location: fbObject.location,
+								pastEvent: fbObject.pastEvent
+							};
+						default:
+							return commonDetails;
+					}
+				};
+
+				if (fbType === 'videos') return acc;
+
+				const fbDetails = fbObjects.data || fbObjects.results;
+
+				if (!acc[fbType]) {
+					acc[fbType] = [];
+				}
+
+				acc[fbType].push(...fbDetails.map(getFbDetails));
+
+				return acc;
+			}, {});
 		} catch (err) {
 			console.error('Pagination Error:', err);
 		} finally {
@@ -44,53 +133,24 @@
 		<Card.Root class="col-span-4">
 			<Card.Content>
 				{#if !loader}
-					{#if posts.length == 0}
+					{#if posts.length === 0}
 						<NotFound message={'No data found!!'} />
 					{:else}
-						<Table.Root>
-							<Table.Header>
-								<Table.Row>
-									<Table.Head>User</Table.Head>
-									<Table.Head>Description</Table.Head>
-									<Table.Head>Date</Table.Head>
-									<Table.Head>Likes</Table.Head>
-								</Table.Row>
-							</Table.Header>
-							<Table.Body>
-								{#each posts as post}
-									<Table.Row>
-										<Table.Cell class="font-medium">
-											<a
-												href={post.actors?.[0]?.url || '#'}
-												target="_blank"
-												class="flex items-center gap-2"
-											>
-												<Avatar.Root class="h-8 w-8">
-													<Avatar.Image src={post.actors?.[0]?.profile_picture} alt="user" />
-													<Avatar.Fallback>{post.actors?.[0]?.name}</Avatar.Fallback>
-												</Avatar.Root>
-												{post.actors?.[0]?.name || 'Unknown'}
-											</a>
-										</Table.Cell>
-										<Table.Cell>
-											<a href={post.url || '#'} target="_blank">
-												{post.message || 'No description'}
-											</a>
-										</Table.Cell>
-										<Table.Cell>
-											{formatDate(post.creation_time)}
-										</Table.Cell>
-										<Table.Cell>
-											{post.feedback?.reaction_count || 0}
-										</Table.Cell>
-									</Table.Row>
-								{/each}
-							</Table.Body>
-						</Table.Root>
+						<div class="columns-2 gap-4 p-4 space-y-4">
+							{#each Object.keys(posts) as key}
+								{#if posts[key].length > 0}
+									{#each posts[key] as post}
+										<div class="break-inside-avoid bg-white shadow-lg rounded-2xl p-4">
+											<FacebookPostCollection {post} />
+										</div>
+									{/each}
+								{/if}
+							{/each}
+						</div>
 					{/if}
 				{:else}
-					<div class="w-full">
-						<TableShimmer rows={15} />
+					<div class="w-full grid grid-cols-2 gap-4 p-4">
+						<CardShimmer rows={16} />
 					</div>
 				{/if}
 			</Card.Content>
