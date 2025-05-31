a560633e20e29fd4d8fe4580eeeb4f8f2c205c54
Vivek Sharma <er.viveksharma2008@gmail.com>
2025-05-31 12:05:53 +0530

    Update layout classes in FooterGuest and HeaderGuest components for consistency

--- src/lib/components/layout/FooterGuest.svelte
+++ src/lib/components/layout/FooterGuest.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="p-4 sm:p-6 dark:bg-gray-800">
-	<div class="mx-auto max-w-screen-xl">
+	<div class="mx-auto container">
 		<div class="md:flex md:justify-between">
 			<div class="mb-6 md:mb-0">
 				<a href="/" class="flex items-center">
